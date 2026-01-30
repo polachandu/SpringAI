c1e13347f3466ac8a9a5259e7c1ceff449ac9b2f
polachandrahas@gmail.com <polachandrahas@gmail.com>
2026-01-29 18:41:23 -0800

    Prompt Template

---
+++ src/main/java/com/example/openai/controller/PromptTemplateController.java
@@ -0,0 +1,30 @@
+package com.example.openai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/springTemplate")
+public class PromptTemplateController {
+
+    @Autowired
+    @Qualifier("chatClient")
+    private ChatClient chatClient;
+
+    @Value("classpath:/promptTemplates/UserPromptTemplate.st")
+    Resource userPromptTemplate;
+
+    @GetMapping("/email")
+    public String emailResponse(@RequestParam String customerName, @RequestParam String customerMessage) {
+        return chatClient.prompt()
+                .system("You are a professional customer service assistance which"
+                        + " helps in drafting email responses to improve productivity of customer support team")
+                .user(promptTemplateSpec -> promptTemplateSpec.text(userPromptTemplate)
+                        .param("customerName", customerName).param("customerMessage", customerMessage))
+                .call().content();
+    }
+}
