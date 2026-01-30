3251538906e5504c32bd6f406de7b2a452060bae
polachandrahas@gmail.com <polachandrahas@gmail.com>
2026-01-29 17:46:11 -0800

    Committing the fucntioning code

--- src/main/java/com/example/openai/config/ChatClientConfig.java
+++ src/main/java/com/example/openai/config/ChatClientConfig.java
@@ -1,7 +1,19 @@
 package com.example.openai.config;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ChatClientConfig {
+
+    @Bean
+    @Primary
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder.defaultSystem(
+                "You are an agriculture specialist. Your role is to help people with questions related to soil, season, fruits and vegetables"
+                        + "If any user asks outside of these topics, inform them to consult chatgpt ")
+                .defaultUser("How can you help me").build();
+    }
 }
--- src/main/java/com/example/openai/controller/OpenAiChatController.java
+++ src/main/java/com/example/openai/controller/OpenAiChatController.java
@@ -1,6 +1,8 @@
 package com.example.openai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,17 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/openaiapi")
 public class OpenAiChatController {
 
-    private final ChatClient chatClient;
-
-    public OpenAiChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.defaultSystem("Act like a motivator and inspire me to keep learn and earn")
-                .build();
-    }
+    @Autowired
+    @Qualifier("chatClient")
+    private ChatClient chatClient;
 
     @GetMapping("/chat")
     public String chat(@RequestParam("message") String message) {
-        return chatClient.prompt()
-                // .system - using system here overrides the defaultSystem
-                .user(message).call().content();
+        return chatClient.prompt().user(message).call().content();
     }
 }
