607059d79857a7a81c33db1035a6c1a3f2517cd2
polachandrahas@gmail.com <polachandrahas@gmail.com>
2026-03-09 18:34:22 -0700

    Added Advisor

--- src/main/java/com/example/openai/config/ChatClientConfig.java
+++ src/main/java/com/example/openai/config/ChatClientConfig.java
@@ -1,6 +1,7 @@
 package com.example.openai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,7 +12,9 @@ public class ChatClientConfig {
     @Bean
     @Primary
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-        return chatClientBuilder.defaultSystem(
+        return chatClientBuilder
+        .defaultAdvisors(new SimpleLoggerAdvisor())
+        .defaultSystem(
                 "You are an agriculture specialist. Your role is to help people with questions related to soil, season, fruits and vegetables"
                         + "If any user asks outside of these topics, inform them to consult chatgpt ")
                 .defaultUser("How can you help me").build();
