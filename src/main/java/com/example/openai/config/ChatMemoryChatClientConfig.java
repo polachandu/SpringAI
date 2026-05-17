a1fab2a46f52f4ec0206f3d492d6595bc1d2114f
polachandrahas@gmail.com <polachandrahas@gmail.com>
2026-05-16 22:16:25 -0700

    Implemented RAG with random text

--- src/main/java/com/example/openai/config/ChatMemoryChatClientConfig.java
+++ src/main/java/com/example/openai/config/ChatMemoryChatClientConfig.java
@@ -1,18 +1,14 @@
 package com.example.openai.config;
 
-import com.example.openai.advisor.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
-import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import java.util.List;
 
